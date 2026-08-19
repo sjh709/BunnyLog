6f87878b24b07ccbd73d82139322975d46b6da6b
Jihee <sjh709@naver.com>
2026-08-19 15:22:03 +0900

    Initial backend setup

---
+++ bunnylog-backend/src/main/java/com/bunnylog/backend/BunnylogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bunnylog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnylogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnylogBackendApplication.class, args);
+	}
+
+}
---
+++ bunnylog-backend/src/test/java/com/bunnylog/backend/BunnylogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunnylog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnylogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
