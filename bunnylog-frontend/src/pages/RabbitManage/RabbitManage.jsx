69a71deb3510f8f829f8afd79c66651860f1e27d
Jihee <sjh709@naver.com>
2026-09-13 15:01:38 +0900

    토끼 관리 페이지

--- bunnylog-frontend/src/pages/RabbitManage/RabbitManage.jsx
+++ bunnylog-frontend/src/pages/RabbitManage/RabbitManage.jsx
@@ -1,12 +1,34 @@
 import PageHeader from '../../components/PageHeader/PageHeader';
 import { Plus } from 'lucide-react';
+import { useState, useEffect } from 'react';
 import './RabbitManage.css';
 import RabbitCard from '../../components/RabbitCard/RabbitCard';
 import BottomNav from '../../components/BottomNav/BottomNav';
 import { useNavigate } from 'react-router-dom';
+import { getRabbits } from '../../api/rabbitApi';
 
 function RabbitManage() {
+  const [rabbits, setRabbits] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchRabbits = async () => {
+      const deviceId = localStorage.getItem('deviceId');
+
+      if (!deviceId) {
+        return;
+      }
+
+      try {
+        const data = await getRabbits(deviceId);
+        setRabbits(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRabbits();
+  }, []);
 
   return (
     <div className='rabbit-container'>
@@ -19,8 +41,11 @@ function RabbitManage() {
         }
       />
 
-      <RabbitCard />
-      <RabbitCard />
+      <div className='rabbit-list'>
+        {rabbits.map((rabbit) => (
+          <RabbitCard rabbit={rabbit} key={rabbit.id} />
+        ))}
+      </div>
 
       <BottomNav />
     </div>
